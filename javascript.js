d7cb3eee7d4aadf0cd914a0452391ec393f85914
SarGGar <garrowsg@gmail.com>
2024-05-13 17:49:57 +0200

    Equal sign returns same display value

--- javascript.js
+++ javascript.js
@@ -53,6 +53,7 @@ buttonSelectors.addEventListener('click', (event) => {
     }
 
     if (target.id == "equals") {
+        console.log(`Operator:${operator}, Number1:${number1}, Number2:${number2}`)
         if (number1!= null && operator!=null && number2==null) {
             number2 = number1;
             let resultValue = operate(Number(number1), Number(number2), operator);
@@ -69,6 +70,8 @@ buttonSelectors.addEventListener('click', (event) => {
             number1 = resultValue;
             number2 = null;
             operator = null;
+        } else if (number1!=null && operator==null && number2==null) {
+            updateDisplay(number1);
         }
 
     }
